fd7b6282875dc74b5490bba603db79b15d261271
Agustín <agustin.gugliotta@globant.com>
2015-04-07 11:42:30 -0300

    Add the BroadCast Receiver for the connection
    * Create the ConnectionBroadcastReceiver
    * Register and unregister the receiver on the BaseActivity

--- Events/eventscorelib/src/main/java/com/globant/eventscorelib/baseComponents/BaseActivity.java
+++ Events/eventscorelib/src/main/java/com/globant/eventscorelib/baseComponents/BaseActivity.java
@@ -1,10 +1,36 @@
 package com.globant.eventscorelib.baseComponents;
 
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 
 /**
  * Created by ignaciopena on 4/1/15.
  */
 public class BaseActivity extends ActionBarActivity {
+
+    BroadcastReceiver mReceiver;
+
+    @Override
+    public void setContentView(View view) {
+        //super.onCreateView();
+        super.setContentView(view);
+    }
+
+    @Override
+    protected void onResume() {
+        mReceiver = new ConnectionBroadcastReceiver();
+        registerReceiver(mReceiver,
+                         new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE"));
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(mReceiver);
+        super.onPause();
+    }
 }
