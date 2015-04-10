15093d0b2879d4df82cc2eb42e201e8822df66e1
Ariel Cattáneo <ariel.cattaneo@globant.com>
2015-04-10 11:58:45 -0300

    Started an abstract DatabaseController

--- Events/eventscorelib/src/main/java/com/globant/eventscorelib/baseComponents/DatabaseController.java
+++ Events/eventscorelib/src/main/java/com/globant/eventscorelib/baseComponents/DatabaseController.java
@@ -1,8 +1,20 @@
 package com.globant.eventscorelib.baseComponents;
 
+import com.globant.eventscorelib.domainObjects.Event;
+
+import java.util.List;
+
 /**
  * Created by ariel.cattaneo on 10/04/2015.
  */
 abstract public class DatabaseController {
     abstract void init();
+
+    abstract public List<Event> getEvents();
+
+    abstract public Event getEvent(String eventId);
+
+    abstract public void addEvent(Event event);
+
+    // TODO: Methods to access the rest of the objects
 }
