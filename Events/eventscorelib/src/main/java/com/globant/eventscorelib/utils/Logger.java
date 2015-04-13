a0876a9132680a6c08ccb96f27698babf317f2d7
Ignacio Peña <ignacio.pena@globant.com>
2015-04-13 13:38:23 -0300

    minor fixes on basecomponents

--- Events/eventscorelib/src/main/java/com/globant/eventscorelib/utils/Logger.java
+++ Events/eventscorelib/src/main/java/com/globant/eventscorelib/utils/Logger.java
@@ -5,7 +5,7 @@ import android.util.Log;
 public class Logger {
 
     public static void e(String message, Exception exception){
-        Log.e(CoreConstants.LOG_TAG, message + " - " + exception);
+        Log.e(CoreConstants.LOG_TAG, message + " - " , exception);
     }
 
     public static void d(String message){
