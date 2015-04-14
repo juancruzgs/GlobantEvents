6da72641acf84bb6a6fbfb26943f6e1d17452266
Juan Cruz <juancruz_gs@hotmail.com>
2015-04-14 14:27:08 -0300

    Toolbar. Search text.

--- Events/eventscorelib/src/main/java/com/globant/eventscorelib/MapActivity.java
+++ Events/eventscorelib/src/main/java/com/globant/eventscorelib/MapActivity.java
@@ -1,15 +1,20 @@
 package com.globant.eventscorelib;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.globant.eventscorelib.baseComponents.BaseActivity;
 import com.globant.eventscorelib.baseComponents.BaseFragment;
@@ -30,7 +35,6 @@ import java.util.List;
 public class MapActivity extends ActionBarActivity implements OnMapReadyCallback {
 
     private GoogleMap mGoogleMap;
-    private EditText mEditTextSearch;
     private Marker mMarker;
 
     @Override
@@ -43,32 +47,17 @@ public class MapActivity extends ActionBarActivity implements OnMapReadyCallback
               .commit();
         mapFragment.getMapAsync(this);
 
-        mEditTextSearch = (EditText)findViewById(R.id.et_location);
-        Button button = (Button)findViewById(R.id.btn_find);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Geocoder geocoder = new Geocoder(getBaseContext());
-                List<Address> addresses;
-                String searchText = mEditTextSearch.getText().toString();
-                try {
-                    addresses = geocoder.getFromLocationName(searchText, 1);
-                    Address address = addresses.get(0);
-
-                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
-                    addMarkerToMap(latLng);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("Map");
+        TextView textView = (TextView)toolbar.findViewById(R.id.toolbar_fragment_title);
+        textView.setText("");
+        setSupportActionBar(toolbar);
     }
 
     private void addMarkerToMap(LatLng latLng) {
         mGoogleMap.clear();
         mMarker = mGoogleMap.addMarker(new MarkerOptions()
-                .position(latLng)
-                .draggable(true));
+                .position(latLng));
                 //.title("Marker"));
         changeCameraPosition(latLng);
         //TODO Set Result
@@ -87,6 +76,9 @@ public class MapActivity extends ActionBarActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
         mGoogleMap.setMyLocationEnabled(true);
+        UiSettings uiSettings = mGoogleMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
+        uiSettings.setCompassEnabled(true);
         mGoogleMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng latLng) {
@@ -99,21 +91,37 @@ public class MapActivity extends ActionBarActivity implements OnMapReadyCallback
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_map, menu);
+//        Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setQueryHint("Search a location");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                Geocoder geocoder = new Geocoder(getBaseContext());
+                List<Address> addresses;
+                try {
+                    //TODO AsyncTask
+                    addresses = geocoder.getFromLocationName(s, 1);
+                    Address address = addresses.get(0);
+
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    addMarkerToMap(latLng);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
