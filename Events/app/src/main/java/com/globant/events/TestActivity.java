b706e695ef459d613e547a2dbe21c1522d2b6f70
Paula Baudo <paula.baudo@globant.com>
2015-04-14 15:46:48 -0300

    EventsClientApplication. Getting event by Id

--- Events/app/src/main/java/com/globant/events/TestActivity.java
+++ Events/app/src/main/java/com/globant/events/TestActivity.java
@@ -1,14 +1,22 @@
 package com.globant.events;
 
+import android.content.Intent;
+import android.hardware.camera2.TotalCaptureResult;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.globant.eventscorelib.Database;
 import com.globant.eventscorelib.baseComponents.BaseActivity;
 import com.globant.eventscorelib.baseComponents.BaseFragment;
+import com.globant.eventscorelib.domainObjects.Event;
+import com.globant.eventscorelib.utils.CoreConstants;
+import com.parse.ParseException;
 
 
 public class TestActivity extends BaseActivity {
@@ -70,10 +78,38 @@ public class TestActivity extends BaseActivity {
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_test, container, false);
             hideUtilsAndShowContentOverlay();
+
+            Button buttonScan = (Button) rootView.findViewById(R.id.button);
+            buttonScan.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intentScan = new Intent(CoreConstants.INTENT_SCAN);
+                    startActivityForResult(intentScan,0);
+                }
+            });
             return rootView;
         }
         @Override
         public String getTitle(){ return "Fragment";
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if (requestCode == 0) {
+                if (resultCode == RESULT_OK) {
+                    String contents = data.getStringExtra(CoreConstants.SCAN_RESULT);
+                    Event event = new Event();
+                    Database database = new Database();
+                    try {
+                        event = database.getEvent(contents);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(getActivity(), event.getTitle(), Toast.LENGTH_SHORT).show();
+                } else if (resultCode == RESULT_CANCELED) {
+                    // Handle Cancel
+                }
+            }
         }
     }
 }
