344bf7e26654478c488c1cd602d363edaea789d7
Agustin, Madina <agustinmadina@gmail.com>
2015-04-16 16:53:00 -0300

    SubscriberFragment finished, retrievepreferences TODO, english missing

--- Events/eventscorelib/src/main/java/com/globant/eventscorelib/fragments/SubscriberFragment.java
+++ Events/eventscorelib/src/main/java/com/globant/eventscorelib/fragments/SubscriberFragment.java
@@ -1,46 +1,53 @@
-package com.globant.eventscorelib.baseComponents;
+package com.globant.eventscorelib.fragments;
 
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.globant.eventscorelib.R;
+import com.globant.eventscorelib.baseComponents.BaseFragment;
+import com.globant.eventscorelib.baseComponents.SharedPreferenceManager;
 import com.software.shell.fab.ActionButton;
-import com.software.shell.fab.FloatingActionButton;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ProfileFragment extends BaseFragment {
+public class SubscriberFragment extends BaseFragment {
 
-    public final static Integer REQUEST_CODE = 0;
+
     Bitmap mPhoto;
-    byte[] mImage;
     ImageView mPhotoProfile;
     ActionButton mFloatingActionButtonPhoto;
+    EditText mEditTextFirstName;
+    EditText mEditTextLastName;
+    EditText mEditTextPhone;
+    EditText mEditTextOccupation;
+    EditText mEditTextEmail;
+    EditText mEditTextTwitter;
+    EditText mEditTextCountry;
+    EditText mEditTextCity;
 
     final int CAMERA_CAPTURE = 1;
     final int CROP_PIC = 2;
-    private Uri picUri;
 
-    public ProfileFragment() {
+    public SubscriberFragment() {
         // Required empty public constructor
     }
 
@@ -49,16 +56,28 @@ public class ProfileFragment extends BaseFragment {
     public View onCreateEventView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView= inflater.inflate(R.layout.fragment_profile, container, false);
-        mFloatingActionButtonPhoto=(ActionButton)rootView.findViewById(R.id.fab);
-        mPhotoProfile=(ImageView)rootView.findViewById(R.id.header);
+        View rootView= inflater.inflate(R.layout.fragment_subscriber, container, false);
+        wireUpViews(rootView);
         prepareImageButton();
         hideUtilsAndShowContentOverlay();
         return rootView;
     }
 
+    private void wireUpViews(View rootView) {
+        mEditTextFirstName=(EditText)rootView.findViewById(R.id.edit_text_first_name);
+        mEditTextLastName=(EditText)rootView.findViewById(R.id.edit_text_last_name);
+        mEditTextPhone=(EditText)rootView.findViewById(R.id.edit_text_phone);
+        mEditTextOccupation=(EditText)rootView.findViewById(R.id.edit_text_email);
+        mEditTextTwitter=(EditText)rootView.findViewById(R.id.edit_text_twitter);
+        mEditTextEmail=(EditText)rootView.findViewById(R.id.edit_text_email);
+        mEditTextCountry=(EditText)rootView.findViewById(R.id.edit_text_country);
+        mEditTextCity=(EditText)rootView.findViewById(R.id.edit_text_city);
+        mFloatingActionButtonPhoto=(ActionButton)rootView.findViewById(R.id.fab);
+        mPhotoProfile=(ImageView)rootView.findViewById(R.id.header);
+    }
+
     @Override
-    public String getTitle() {
+    public String getFragmentTitle() {
         return null;
     }
 
@@ -73,7 +92,7 @@ public class ProfileFragment extends BaseFragment {
                 // get the returned data
                 Bundle extras = data.getExtras();
                 // get the cropped bitmap
-                Bitmap mPhoto = extras.getParcelable("data");
+                mPhoto = extras.getParcelable("data");
                 mPhotoProfile.setImageBitmap(mPhoto);
 
             }
@@ -115,6 +134,43 @@ public class ProfileFragment extends BaseFragment {
             // start the activity - we handle returning in onActivityResult
             startActivityForResult(cropIntent, CROP_PIC);
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_subscriber, menu);
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_done) {
+            SharedPreferenceManager.setUserFirstName(mEditTextFirstName.getText().toString(), this.getActivity());
+            SharedPreferenceManager.setUserLastName(mEditTextLastName.getText().toString(), this.getActivity());
+            SharedPreferenceManager.setUserPhone(mEditTextPhone.getText().toString(), this.getActivity());
+            SharedPreferenceManager.setUserOccupation(mEditTextOccupation.getText().toString(), this.getActivity());
+            SharedPreferenceManager.setUserEmail(mEditTextEmail.getText().toString(), this.getActivity());
+            SharedPreferenceManager.setUserCountry(mEditTextCountry.getText().toString(), this.getActivity());
+            SharedPreferenceManager.setUserCity(mEditTextCity.getText().toString(), this.getActivity());
+            SharedPreferenceManager.setUserTwitter(mEditTextTwitter.getText().toString(), this.getActivity());
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 }
 
