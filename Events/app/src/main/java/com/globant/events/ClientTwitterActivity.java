96573a1fa663f6a620ceb55ee2c5afec78a9b508
micaela.cavallo <micaela.cavallo@globant.com>
2015-04-20 09:12:21 -0300

    Layout fragment_tweet ready

---
+++ Events/app/src/main/java/com/globant/events/ClientTwitterActivity.java
@@ -0,0 +1,14 @@
+package com.globant.events;
+
+
+import android.os.Bundle;
+
+import com.globant.eventscorelib.baseComponents.BaseTwitterActivity;
+
+public class ClientTwitterActivity extends BaseTwitterActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
