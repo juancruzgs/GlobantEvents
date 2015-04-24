a025de67016b8088b9d301a5b0aedec8c17d9881
Paula Baudo <paula.baudo@globant.com>
2015-04-24 17:00:18 -0300

    New Card Layout

--- Events/eventscorelib/src/main/java/com/globant/eventscorelib/baseComponents/BaseEventsListViewHolder.java
+++ Events/eventscorelib/src/main/java/com/globant/eventscorelib/baseComponents/BaseEventsListViewHolder.java
@@ -1,7 +1,11 @@
 package com.globant.eventscorelib.baseComponents;
 
+import android.graphics.drawable.Drawable;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.globant.eventscorelib.R;
@@ -15,6 +19,25 @@ public class BaseEventsListViewHolder extends RecyclerView.ViewHolder{
     public BaseEventsListViewHolder(final View itemView) {
         super(itemView);
         mTextView = (TextView) itemView.findViewById(R.id.event_title_text_view);
+    }
+
+    private void hideSpeakersLayout(View itemView) {
+        LinearLayout linearLayoutSpeakers = (LinearLayout) itemView.findViewById(R.id.speakers_layout);
+        linearLayoutSpeakers.setVisibility(View.GONE);
+    }
+
+    private void showSpeakersLayout(View itemView) {
+        LinearLayout linearLayoutSpeakers = (LinearLayout) itemView.findViewById(R.id.speakers_layout);
+        linearLayoutSpeakers.setVisibility(View.VISIBLE);
+    }
+
+    private void getGreenSpeakerIcon(View itemView) {
+        Drawable drawableToApply = DrawableCompat.wrap(itemView.getResources().getDrawable(R.mipmap.ic_speaker));
+        DrawableCompat.setTint(drawableToApply, itemView.getResources().getColor(R.color.globant_green));
+        drawableToApply = DrawableCompat.unwrap(drawableToApply);
+        ImageView speakerIcon = (ImageView) itemView.findViewById(R.id.speaker_image_view);
+        speakerIcon.setImageDrawable(drawableToApply);
+        speakerIcon.setImageDrawable(itemView.getResources().getDrawable(R.mipmap.ic_speaker));
     }
 
     public TextView getTextView() {
