5d9cf08a6850ea985f73f496785b0c96fff02551
Agustín <agustin.gugliotta@globant.com>
2015-04-27 10:41:48 -0300

    Change ActionBarActivity to AppCompatActivity and add change to SpeakerList

--- Events/eventscorelib/src/main/java/com/globant/eventscorelib/baseFragments/BaseSpeakersListFragment.java
+++ Events/eventscorelib/src/main/java/com/globant/eventscorelib/baseFragments/BaseSpeakersListFragment.java
@@ -48,7 +48,7 @@ public class BaseSpeakersListFragment extends BaseFragment implements BaseServic
 
     @Override
     public void onStartAction(BaseService.ACTIONS theAction) {
-        showProgressOverlay("Loading Speakers");
+
     }
 
     @Override
@@ -63,15 +63,13 @@ public class BaseSpeakersListFragment extends BaseFragment implements BaseServic
 
     @Override
     public void onFailAction(BaseService.ACTIONS theAction, Exception e) {
+        showErrorOverlay();
     }
 
     public BaseSpeakersListFragment() {
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+
 
     @Override
     public void setService(BaseService service) {
@@ -88,6 +86,7 @@ public class BaseSpeakersListFragment extends BaseFragment implements BaseServic
     protected View onCreateEventView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         int scrollPosition = 0;
         View rootView = inflater.inflate(R.layout.fragment_speaker_list, container, false);
+
         mRecyclerView = (RecyclerView) rootView.findViewById(R.id.speaker_list_recycler_view);
         mLayoutManager = new LinearLayoutManager(getActivity());
         mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
@@ -104,9 +103,11 @@ public class BaseSpeakersListFragment extends BaseFragment implements BaseServic
         mRecyclerView.setAdapter(mAdapter);
 
         mRecyclerView.setHasFixedSize(true);
-
-        hideUtilsAndShowContentOverlay();
-
+        if (mAdapter.getItemCount() == 0){
+            showProgressOverlay();
+        } else {
+            hideUtilsAndShowContentOverlay();
+        }
         mRecyclerView.addOnItemTouchListener(
                 new RecyclerItemClickListener(getActivity(), new RecyclerItemClickListener.OnItemClickListener() {
                     @Override public void onItemClick(View view, int position) {
