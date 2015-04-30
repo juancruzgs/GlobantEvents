fb6530b9af7c187b8fcc28e8d54bc3bbfa1e0cd8
Paula Baudo <paula.baudo@globant.com>
2015-04-30 14:36:26 -0300

    Up button in credits screen

--- Events/eventscorelib/src/main/java/com/globant/eventscorelib/baseActivities/BaseCreditsActivity.java
+++ Events/eventscorelib/src/main/java/com/globant/eventscorelib/baseActivities/BaseCreditsActivity.java
@@ -1,10 +1,12 @@
 package com.globant.eventscorelib.baseActivities;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.globant.eventscorelib.R;
-import com.globant.eventscorelib.baseActivities.BaseActivity;
 import com.globant.eventscorelib.baseFragments.BaseCreditsFragment;
+import com.globant.eventscorelib.controllers.SharedPreferencesController;
 
 
 public class BaseCreditsActivity extends BaseActivity {
@@ -13,10 +15,22 @@ public class BaseCreditsActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+        prepareToolbar();
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new BaseCreditsFragment())
                     .commit();
         }
     }
+
+    private void prepareToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (SharedPreferencesController.getUserFirstName(this) != null) {
+            getSupportActionBar().setTitle(SharedPreferencesController.getUserFirstName(this) + " " + SharedPreferencesController.getUserLastName(this));
+        }
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setIcon(new ColorDrawable(getResources().getColor(android.R.color.transparent)));
+    }
+
 }
