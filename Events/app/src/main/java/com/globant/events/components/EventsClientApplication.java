e0e7a09734c86099fcd1d85df98f1d83fe4246d9
Agustín <agustin.gugliotta@globant.com>
2015-04-30 08:47:02 -0300

    Add reception of push notifications for the client app

--- Events/app/src/main/java/com/globant/events/components/EventsClientApplication.java
+++ Events/app/src/main/java/com/globant/events/components/EventsClientApplication.java
@@ -2,10 +2,6 @@ package com.globant.events.components;
 
 import com.globant.eventscorelib.baseComponents.BaseApplication;
 import com.globant.eventscorelib.baseComponents.BaseService;
-import com.globant.eventscorelib.utils.Logger;
-import com.parse.ParseException;
-import com.parse.ParsePush;
-import com.parse.SaveCallback;
 
 /**
  * Created by paula.baudo on 4/14/2015.
@@ -14,20 +10,5 @@ public class EventsClientApplication extends BaseApplication {
     @Override
     public Class<? extends BaseService> getServiceClass() {
         return DataServiceClient.class;
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        ParsePush.subscribeInBackground("", new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if (e == null) {
-                    Logger.d("successfully subscribed to the broadcast channel.");
-                } else {
-                    Logger.e("failed to subscribe for push", e);
-                }
-            }
-        });
     }
 }
