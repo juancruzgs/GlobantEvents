1f610bef19c5d809486ae51f75450a769ea7bc5b
Agustín <agustin.gugliotta@globant.com>
2015-05-11 11:56:47 -0300

    Fix EventDescriptionManagerFragment

--- Events/eventmanager/src/main/java/com/globant/eventmanager/fragments/EventDescriptionManagerFragment.java
+++ Events/eventmanager/src/main/java/com/globant/eventmanager/fragments/EventDescriptionManagerFragment.java
@@ -3,8 +3,8 @@ package com.globant.eventmanager.fragments;
 import android.content.Intent;
 import android.view.MenuItem;
 
-import com.globant.eventmanager.activities.MapManagerActivity;
 import com.globant.eventmanager.activities.PushNotificationActivity;
+import com.globant.eventscorelib.baseActivities.BaseEventDetailPagerActivity;
 import com.globant.eventscorelib.baseComponents.BaseApplication;
 import com.globant.eventscorelib.baseFragments.BaseEventDescriptionFragment;
 
@@ -16,18 +16,12 @@ public class EventDescriptionManagerFragment extends BaseEventDescriptionFragmen
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == com.globant.eventscorelib.R.id.action_map) {
-            Intent intent = new Intent(getActivity(), MapManagerActivity.class);
-            startActivity(intent);
-            return true;
-        } else {
-            if (id == com.globant.eventscorelib.R.id.action_notifications){
-                Intent intentNotifications = new Intent(getActivity(), PushNotificationActivity.class);
-                intentNotifications.putExtra(PushNotificationFragment.SOURCE_TAG,
-                                            BaseApplication.getInstance().getEvent().getObjectID());
-                startActivity(intentNotifications);
-                return  true;
-            }
+        if (id == com.globant.eventscorelib.R.id.action_notifications){
+            Intent intentNotifications = new Intent(getActivity(), PushNotificationActivity.class);
+            intentNotifications.putExtra(PushNotificationFragment.SOURCE_TAG,
+                    BaseEventDetailPagerActivity.getInstance().getEvent().getObjectID());
+            startActivity(intentNotifications);
+            return  true;
         }
         return false;
     }
