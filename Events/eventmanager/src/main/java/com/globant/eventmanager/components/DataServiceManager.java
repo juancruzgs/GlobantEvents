06d3b50793916f2bd39242590062463f4ec1cad2
Juan Cruz <juancruz_gs@hotmail.com>
2015-05-11 10:47:24 -0300

    Merge Pager cache
    Conflicts:
    	Events/eventmanager/src/main/java/com/globant/eventmanager/fragments/EventDescriptionManagerFragment.java

--- Events/eventmanager/src/main/java/com/globant/eventmanager/components/DataServiceManager.java
+++ Events/eventmanager/src/main/java/com/globant/eventmanager/components/DataServiceManager.java
@@ -1,9 +1,15 @@
 package com.globant.eventmanager.components;
 
 import com.globant.eventscorelib.baseComponents.BaseService;
+import com.globant.eventscorelib.utils.CoreConstants;
 
 /**
  * Created by ariel.cattaneo on 13/04/2015.
  */
 public class DataServiceManager extends BaseService {
+
+    @Override
+    protected String getTwitterCallbackURL() {
+        return CoreConstants.TWITTER_CALLBACK_URL_MANAGER;
+    }
 }
