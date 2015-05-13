bd6ede6d84c77a794aabecacfdbdcd7ad026786d
Juan Cruz <juancruz_gs@hotmail.com>
2015-05-13 15:03:34 -0300

    Refactor

--- Events/eventscorelib/src/main/java/com/globant/eventscorelib/controllers/LocalDataController.java
+++ Events/eventscorelib/src/main/java/com/globant/eventscorelib/controllers/LocalDataController.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-abstract public class LocalDataController {
+public final class LocalDataController {
 
     private LocalDataController() {
     }
